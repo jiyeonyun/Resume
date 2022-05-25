1ae3689231a3607b678cf6c3245c7865198a5a68
jiyeonyun <jiyeonyun43@gmail.com>
2022-05-25 16:01:10 +0900

    personalnfo component data

--- resumes/src/App.jsx
+++ resumes/src/App.jsx
@@ -6,15 +6,16 @@ import PersonalInfo from "./components/personalInfo/personalInfo";
 function App() {
   const [personalInfoData, setPersonalInfoData] = useState({
     name: "홍길동",
+    nameh: "洪吉同",
     birth: "1998-03-18",
-    adress: "서울특별시 영등포구",
+    adress: "서울특별시 영등포구 당산로 3213 2020호",
     email: "jyyun@zentropy.com",
     number: "010-2055-7663",
     age: 24,
   });
   return (
     <div className="App">
-      <PersonalInfo />
+      <PersonalInfo personalInfoData={personalInfoData} />
     </div>
   );
 }
--- resumes/src/components/personalInfo/personalInfo.jsx
+++ resumes/src/components/personalInfo/personalInfo.jsx
@@ -4,15 +4,15 @@ const PersonalInfo = ({ personalInfoData }) => {
   return (
     <table border="1" width="500">
       <tr>
-        <td rowspan="5" width="85" height="113">
+        <td rowspan="5" width="85" height="80">
           <img src="" alt="" />
         </td>
-        <th colspan="9" height="50">
+        <th colspan="9" height="30">
           <font size="5">이 력 서</font>
         </th>
       </tr>
       <tr>
-        <th rowspan="3" width="85" height="70" bgcolor="D5D5D5">
+        <th rowspan="3" width="85" height="50" bgcolor="D5D5D5">
           성 명
         </th>
       </tr>
@@ -20,7 +20,7 @@ const PersonalInfo = ({ personalInfoData }) => {
         <th colspan="2" bgcolor="D5D5D5">
           한 글
         </th>
-        <td width="90"></td>
+        <td width="90">{personalInfoData.name}</td>
         <th bgcolor="D5D5D5">생 년 월 일</th>
         <th colspan="2" width="80" bgcolor="D5D5D5">
           E-mail
@@ -30,23 +30,23 @@ const PersonalInfo = ({ personalInfoData }) => {
         <th colspan="2" bgcolor="D5D5D5">
           한 문
         </th>
-        <td></td>
-        <td></td>
-        <td colspan="2"></td>
+        <td>{personalInfoData.nameh}</td>
+        <td>{personalInfoData.birth}</td>
+        <td colspan="2">{personalInfoData.email}</td>
       </tr>
       <tr>
         <th colspan="2" width="50" bgcolor="D5D5D5">
           연 락 처
         </th>
-        <td colspan="2"></td>
+        <td colspan="2">{personalInfoData.number}</td>
         <th colspan="2" width="50" bgcolor="D5D5D5">
           나이
         </th>
-        <td colspan="2">만 세</td>
+        <td colspan="2">만 {personalInfoData.age} 세</td>
       </tr>
       <tr>
         <th bgcolor="D5D5D5">주 소</th>
-        <td colspan="8"></td>
+        <td colspan="8">{personalInfoData.adress}</td>
       </tr>
     </table>
   );
