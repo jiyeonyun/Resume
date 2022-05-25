43aa4572ea50c66a9788f97c57e3f9698eb8f458
jiyeonyun <jiyeonyun43@gmail.com>
2022-05-25 17:12:51 +0900

    carrerData

---
+++ resumes/src/data/careerData.js
@@ -0,0 +1,47 @@
+const career = [
+  {
+    worgingDays: "1년 3개월",
+    term: "2017/02 ~ 2018/05",
+    company: "젠트로피",
+    position: "사원",
+  },
+  {
+    worgingDays: "1년 5개월",
+    term: "2018/07 ~ 2019/05",
+    company: "가나다",
+    position: "사원",
+  },
+  {
+    worgingDays: "1년 0개월",
+    term: "2020/07 ~ 2021/05",
+    company: "마바사",
+    position: "사원",
+  },
+  {
+    worgingDays: "2년 3개월",
+    term: "2022/08 ~ 2024/09",
+    company: "아자차카",
+    position: "사원",
+  },
+];
+
+const certificate = [
+  {
+    name: "컴퓨터활용능력1급",
+    year: "2017",
+  },
+  {
+    name: "토익",
+    year: "2018",
+  },
+  {
+    name: "정보처리기사",
+    year: "2019",
+  },
+  {
+    name: "전산회계",
+    year: "2022",
+  },
+];
+
+export { career, certificate };
